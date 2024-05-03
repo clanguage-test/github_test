7db3aa88b8cfb28081243d2e63ed6207c3f5bb0d
지호 김 <jisihoho@naver.com>
2024-05-03 10:45:49 +0900

    [01] 김지호

--- github_test/github_test.c
+++ github_test/github_test.c
@@ -8,11 +8,14 @@ int tutee_00() {
 	printf("2024xxxxxx ±è½´´Ï\n");
 }
 
+int tutee_01() {
+	printf("2024111224 ±èÁöÈ£\n");
+}
 
 int main() {
 	tutor_01(); //ÀÌÀº°æ
 	tutee_00();//±è½´´Ï
-	//tutee_01();//±èÁöÈ£
+	tutee_01();// ±èÁöÈ£
 	//tutee_02();//±èÇı¹Î
 	//tutee_03();//¼ÛÁöÀ±
 	//tutee_04();//À¯¿¬¼­
