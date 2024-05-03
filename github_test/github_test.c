c91fe1e7d0a8b4f99bef4214966dff71b270b37b
jiyoo <jiyoo@JY6>
2024-05-03 11:09:07 +0900

    [03]송지윤

--- github_test/github_test.c
+++ github_test/github_test.c
@@ -8,13 +8,17 @@ int tutee_00() {
 	printf("2024xxxxxx ±è½´´Ï\n");
 }
 
+int tutee_03() {
+	printf("2024xxxxxx ¼ÛÁöÀ±\n");
+}
+
 
 int main() {
 	tutor_01(); //ÀÌÀº°æ
 	tutee_00();//±è½´´Ï
 	//tutee_01();//±èÁöÈ£
 	//tutee_02();//±èÇı¹Î
-	//tutee_03();//¼ÛÁöÀ±
+	tutee_03();//¼ÛÁöÀ±
 	//tutee_04();//À¯¿¬¼­
 
 }
